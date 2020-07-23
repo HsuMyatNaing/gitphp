77aa81557cf7df050fe3396c4fe508ae3471f1a5
HsuMyatNaing <67919377+HsuMyatNaing@users.noreply.github.com>
2020-07-23 08:46:20 +0630

    Add files via upload

---
+++ triangleNo.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<body>
+<?php
+echo "Result is";
+echo "<br>";
+		
+
+    for($i=1;$i<=8;$i++){
+        
+        for($j=1;$j<=$i;$j++){
+            $k=$i*$j;
+            echo $k." ";
+        }
+        
+        echo "<br>";
+    }
+	
+?>
+</body>
+</html>
