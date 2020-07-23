e657bf22228ce29112b5654e48e09643e8e2d10e
HsuMyatNaing <67919377+HsuMyatNaing@users.noreply.github.com>
2020-07-23 09:30:21 +0630

    Add files via upload

---
+++ ex11.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<body>
+<?php
+echo "Result is";
+echo "<br>";
+		
+
+    for($i=1;$i<=8;$i++){
+        
+        for($j=1;$j<=$i;$j++){
+            
+            echo $j." ";
+        }
+        
+        echo "<br>";
+    }
+	
+?>
+</body>
+</html>
